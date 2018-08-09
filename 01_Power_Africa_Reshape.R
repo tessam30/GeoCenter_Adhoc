1b19b38c7c3ed6f85aaf902f573530710bc22373
Tessam <tim.essam@gmail.com>
2018-08-08 23:09:08 -0400

    add in code, indent

--- 01_Power_Africa_Reshape.R
+++ 01_Power_Africa_Reshape.R
@@ -17,54 +17,54 @@ library(magrittr)
 
 
 # Where does your data live? You'll have to change this on your machine
+(datapath) 
 data <- c("West Africa_Enabling Environment Scorecard Mockup.xlsx")
-datapath
+
 
 # Load the data
-df <- read_excel(file.path(datapath, data), sheet = "Calculate Percentile") %>% 
-  select(-starts_with("Percentile")) %>% 
-  rename(DB = `DB 2018`)
+  df <- read_excel(file.path(datapath, data), sheet = "Calculate Percentile") %>% 
+    select(-starts_with("Percentile")) %>% 
+    rename(DB = `DB 2018`)
 
 # Check how the data are read in -- do some come in as non-numerical? Not any more.
-str(df)  
+  str(df)  
 
 # Let's get this stuff in shape. First, some gathering.
-df_shaped <- 
-  df %>% 
-  gather(., key = "metric", value = "value", DB:`PPP Procurement`) %>% 
-  group_by(metric) %>% 
-  # Recalculate the grouped percentile rank
-  mutate(percent_rank = percent_rank(value))
+  df_shaped <- 
+    df %>% 
+    gather(., key = "metric", value = "value", DB:`PPP Procurement`) %>% 
+    group_by(metric) %>% 
+    # Recalculate the grouped percentile rank
+    mutate(percent_rank = percent_rank(value))
 
 
 # Export and expand -------------------------------------------------------
 
-write_csv(df_shaped, file.path(datapath, "WAEES_data_2018_0808.csv"))
+  write_csv(df_shaped, file.path(datapath, "WAEES_data_2018_0808.csv"))
 
 
 # One plot for good measure
-df_shaped %>% 
-  mutate(country = as.factor(Economy), 
-         country = fct_reorder(country, -percent_rank)) %>% 
-  ggplot(., aes(x = percent_rank, y = country)) + 
-  geom_point() + 
-  facet_wrap(~ metric) 
+  df_shaped %>% 
+    mutate(country = as.factor(Economy), 
+           country = fct_reorder(country, -percent_rank)) %>% 
+    ggplot(., aes(x = percent_rank, y = country)) + 
+    geom_point() + 
+    facet_wrap(~ metric) 
 
 # One quick PCA to see if it even makes sense -- too much missing data
 
-df_pca <-  
-  df_shaped %>% 
-  select(-percent_rank) %>% 
-  spread(., metric, value) %>% 
+  df_pca <-  
+    df_shaped %>% 
+    select(-percent_rank) %>% 
+    spread(., metric, value) %>% 
+    
+    # set row names, Keep cols for PCA
+    column_to_rownames('Economy') %>% 
+    select(DB:`Utility Creditworthiness`) 
   
-  # set row names, Keep cols for PCA
-  column_to_rownames('Economy') %>% 
-  select(DB:`Utility Creditworthiness`) 
-
-# For later, when you go down the PCA path
-pca_out <- prcomp(na.omit((df_pca)), scale = 
-                    TRUE)
-biplot((pca_out), var.axes = TRUE, scale = 0, cex = c(1, 1))
+  # For later, when you go down the PCA path
+  pca_out <- prcomp(na.omit((df_pca)), scale = TRUE)
+  biplot((pca_out), var.axes = TRUE, scale = 0, cex = c(1, 1))
 
 
 
