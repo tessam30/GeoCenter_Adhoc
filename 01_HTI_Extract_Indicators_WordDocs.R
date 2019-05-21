650bb82fef38d9a34692a206936abc658008d9b1
Tessam <tim.essam@gmail.com>
2019-05-21 11:47:58 -0400

    pull in lists, flatten, extract

--- 01_HTI_Extract_Indicators_WordDocs.R
+++ 01_HTI_Extract_Indicators_WordDocs.R
@@ -1,37 +1,65 @@
 library(docxtractr)
 library(tibble)
 library(tidyverse)
+library(readxl)
 
 
 # Read in the data
-list.files(file.path(datapath), pattern = "PIRS")
-doc <- read_docx(file.path(datapath, "PIRS_TEST.docx"))
-doc2 <- read_docx(file.path(datapath, "Reforestation_Projects_PIRS.docx" ))
+pirs_files <- list.files(file.path(datapath), pattern = "PIRS")
+
+# Read in all the indicator data and store in a list for accessing tables
+pirs_list <- 
+  pirs_files %>% 
+  map(~ read_docx(file.path(datapath, .)))
+
+# Storing all the tabls in a list to purrr over w/ search terms
+pirs_tbls <- 
+  pirs_list %>% 
+  map(., ~docx_extract_all_tbls(.)) %>% 
+  flatten_dfr() 
 
 
-# indic_raw <- tibble(filename = indicator_files) %>% 
-#   mutate(file_contents = map(filename, 
-#                              ~read_docx(file.path(datapath, .)))) %>% 
-#   mutate(filename = str_remove(filename, ".docx")) %>% 
-#   unnest() 
+# Use \\ before paratheses to escape them in the grep call
+match_terms <- c("Indicator Name and Number:", 
+                 "Definition", "Unit of Measure", 
+                 "Disaggregation", "Reporting Level", 
+                 "Data Collection Method:", "Data Source\\(s\\):", 
+                 "Timing/Frequency of Data Acquisition:", 
+                 "Data Analysis:", "Data Review:", 
+                 "Known Data Limitations")
 
 
-docx_tbl_count(doc2)
-docx_describe_tbls(doc2)
+pirs_df <- 
+  pirs_tbls %>% 
+  mutate(test = grepl(regex, V1)) %>% 
+  filter(test %in% TRUE) %>% 
+  select(V1) %>% 
+  separate(V1, into = c("terms", "text"), sep = "[:]", extra = "merge" ) %>% 
+  mutate(text = str_squish(text))
+  
+  
+  
+  write_excel_csv(., file.path(datapath, "tmp_indicators.csv"))
 
-tmp <- docx_extract_all_tbls(doc2) 
 
-%>% 
-  rename(sheet1 = "USAID.Haiti.Activity.Performance.Indicator.Reference.Sheet")
+  
+
 
 # Paste search terms in as vector using Data Pasta
-# Use \\ before paratheses to escape them in the grep call
-match_terms <- c("Indicator Name and Number:", "Definition", "Unit of Measure", "Disaggregation", "Reporting Level", "Data Collection Method:", "Data Source\\(s\\):", "Timing/Frequency of Data Acquisition:", "Data Analysis:", "Data Review:", "Known Data Limitations")
 
 regex = paste(match_terms, collapse="|")
 
 
 # Now, to iterate through each element of the list and see what words match, flag those entries and extract them into a flat file
-tmp <- tmp %>% mutate(test = grepl(regex, sheet1)) %>% print(n = Inf) %>% filter(test %in% "TRUE")
+pirs_tbls[[1]] %>% mutate(test = grepl(regex, V1)) %>% print(n = Inf) %>% filter(test %in% "TRUE")
 
-               
+pirs_tbls %>% 
+  map_df(~ .x %>% mutate_at(vars(V1), list(test = grepl(regex, V1))))
+
+tmp <- map_df(pirs_tbls, ~ mutate(.x, funs(test = grepl(regex, V1))))
+
+
+         ~mutate(test = grepl(regex, .xV1)))
+
+y %>% map(~ .x %>% mutate_at(vars(-X1), funs(case_when(is.na(.) ~ -9999, 
+                                                       TRUE ~ . ))))
