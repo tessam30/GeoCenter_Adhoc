f90d055311772d7326dfeaae8335472f452bd2cf
Tessam <tim.essam@gmail.com>
2019-05-21 07:30:29 -0400

    start extrac files

--- 01_HTI_Extract_Indicators_WordDocs.R
+++ 01_HTI_Extract_Indicators_WordDocs.R
@@ -1,14 +1,27 @@
 library(docxtractr)
 library(tibble)
-library(dplyr)
+library(tidyverse)
+
 
 # Read in the data
-doc <- read_docx(file.path("Data", "PIRS_TEST.docx"))
+list.files(file.path(datapath), pattern = "PIRS")
+doc <- read_docx(file.path(datapath, "PIRS_TEST.docx"))
+doc2 <- read_docx(file.path(datapath, "Reforestation_Projects_PIRS.docx" ))
 
-docx_tbl_count(doc)
-docx_describe_tbls(doc)
 
-tmp <- docx_extract_tbl(doc) %>% 
+# indic_raw <- tibble(filename = indicator_files) %>% 
+#   mutate(file_contents = map(filename, 
+#                              ~read_docx(file.path(datapath, .)))) %>% 
+#   mutate(filename = str_remove(filename, ".docx")) %>% 
+#   unnest() 
+
+
+docx_tbl_count(doc2)
+docx_describe_tbls(doc2)
+
+tmp <- docx_extract_all_tbls(doc2) 
+
+%>% 
   rename(sheet1 = "USAID.Haiti.Activity.Performance.Indicator.Reference.Sheet")
 
 # Paste search terms in as vector using Data Pasta
@@ -17,6 +30,8 @@ match_terms <- c("Indicator Name and Number:", "Definition", "Unit of Measure", 
 
 regex = paste(match_terms, collapse="|")
 
-tmp %>% mutate(test = grepl(regex, sheet1)) %>% print(n = Inf) %>% filter(test %in% "TRUE")
+
+# Now, to iterate through each element of the list and see what words match, flag those entries and extract them into a flat file
+tmp <- tmp %>% mutate(test = grepl(regex, sheet1)) %>% print(n = Inf) %>% filter(test %in% "TRUE")
 
                
